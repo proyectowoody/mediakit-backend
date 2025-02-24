736f9d3dcb2c4256ee34412d26d12302b7f0deed
proyectowoody <proyectowoody@gmail.com>
2025-02-24 00:19:10 -0400

    Cambio para vercel

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('Media kit')
+    .setTitle('respectfull shoes')
     .setDescription(
       'This is the documentation for Media kit.',
     )
