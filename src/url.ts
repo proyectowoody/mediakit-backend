b3c17c1301cbdfdc6d4ab587f0777d8f9f07b1c7
proyectowoody <proyectowoody@gmail.com>
2025-03-23 11:23:48 -0400

    Cambiando la ruta

--- src/url.ts
+++ src/url.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-export const isProduction = process.env.NODE_ENV === "development"; 
+export const isProduction = process.env.NODE_ENV === "production"; 
 
 export const GOOGLE_CLIENT_ID = isProduction
     ? process.env.GOOGLE_CLIENT_ID_PRODUCTION
